5cf59bc1836d758b1fc58c8f1b3277a20481ced5
hieu <hieuvnautotech@gmail.com>
2022-10-19 13:48:55 +0700

    sau khi cấu hình redux saga xong

--- client/src/redux/reducers/posts.js
+++ client/src/redux/reducers/posts.js
@@ -1,3 +1,28 @@
-export default function postsReducer() { 
-    
+import { INIT_STATE } from "../../constant";
+import { getPosts } from "../actions";
+
+export default function postsReducer(state = INIT_STATE.posts, action) { 
+    switch (action.type) {
+      case getType(getPosts.getPostsRequest):
+        // break;
+        return {
+          ...state,
+          isLoading: true,
+        };
+      case getType(getPosts.getPostSuccess):
+        // break;
+        return {
+          ...state,
+          isLoading: false,
+          data: action.payload,
+        };
+      case getType(getPosts.getPostFailure):
+        // break;
+        return {
+          ...state,
+          isLoading: false,
+        };
+      default:
+        return state;
+    }
 }
--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -1,5 +1,13 @@
-function* mySaga() { 
+import { takeLatest, call } from 'redux-saga/effects'
+import * as actions from '../actions'
+import * as api from '../../api'
 
+function* fetchPostSaga(action) { 
+    const posts = yield call(api.fetchPosts)
+    console.log('[posts]', posts)
+}
+function* mySaga() { 
+    yield takeLatest(actions.getPosts.getPostRequest,fetchPostSaga)
 }
 
 export default mySaga;

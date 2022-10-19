8feeb7a4f2c054b6576a29318c4efccfc27a3d58
hieu <hieuvnautotech@gmail.com>
2022-10-19 08:54:57 +0700

    cty

--- server/index.js
+++ server/index.js
@@ -4,16 +4,14 @@ import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const URI = 'mongodb+srv://hieuvnautotech:211899900So@mern-learnit.ygpexhl.mongodb.net/MERNFullStack?retryWrites=true&w=majority'
+
 app.use(bodyParser.json({limit:'30mb'}));
 app.use(cors());
-app.use(express.json({ limit: "30mb" }));
-app.use(express.urlencoded({ extended: true, limit: "30mb" }));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
-app.get('/', (req, res) => {
-    res.send('Welcome')
+app.get('/', (req, res) => { res.send("welcome") });
+
+app.listen(PORT, () => { 
+  console.log(`listening on port + ${PORT}`);
 });
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });

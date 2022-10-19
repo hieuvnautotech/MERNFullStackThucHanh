0d781ea175511a9126c137a9e75380b3be9edc9f
hieu <hieuvnautotech@gmail.com>
2022-10-19 09:25:18 +0700

    tập làm router

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import posts from './routers/posts.js'
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,7 +11,8 @@ app.use(cors());
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
-app.get('/', (req, res) => { res.send("welcome") });
+// app.get('/', (req, res) => { res.send("welcome") });
+app.use("/posts", posts);
 
 app.listen(PORT, () => { 
   console.log(`listening on port + ${PORT}`);

71c98716da954029fe9e9afeaf5955bf9b1dcf81
hieu <hieuvnautotech@gmail.com>
2022-10-19 16:15:48 +0700

    đang làm chưa xong

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Typography } from "@material-ui/core";
 
 export default function Header() {
     return (
-        <div>Header</div>
+      // <div>Header</div>
+        <Typography variant="h4" align="center" className={{}}>
+        Blog
+      </Typography>
     );
 }

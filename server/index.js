1c5ae2d7f09eebdd79ca57e6a3dd33f3653df5ca
hieu <hieuvnautotech@gmail.com>
2022-10-19 09:39:29 +0700

    database connected

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import posts from './routers/posts.js'
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const URI =
+  "mongodb+srv://hieuvnautotech:211899900So@mern-learnit.ygpexhl.mongodb.net/MERNFullStack?retryWrites=true&w=majority";
+ 
 
 app.use(bodyParser.json({limit:'30mb'}));
 app.use(cors());
@@ -13,6 +17,12 @@ app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
 // app.get('/', (req, res) => { res.send("welcome") });
 app.use("/posts", posts);
+
+//kết nối db
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => { 
+    console.log("Connected to Mongoose server");
+  }).catch(err => { console.log('err', err) })
 
 app.listen(PORT, () => { 
   console.log(`listening on port + ${PORT}`);

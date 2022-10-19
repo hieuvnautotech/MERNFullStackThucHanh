7a8e3c0dff260e070f155c6ea39c275571839c95
hieu <hieuvnautotech@gmail.com>
2022-10-19 09:30:57 +0700

    controller

--- server/routers/posts.js
+++ server/routers/posts.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import { getPosts} from '../controllers/posts.js'
 
 const router = express.Router();
 
-router.get('/', (req, res) => { res.send('router success') })
+// router.get('/', (req, res) => { res.send('router success') })
+router.get("/", getPosts);
 
 export default router

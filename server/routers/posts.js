a81808fd9fb270e2700194c38bca0cb4e3ff50e5
hieu <hieuvnautotech@gmail.com>
2022-10-19 10:04:20 +0700

    tạo create post trong controller show ra text demo

--- server/routers/posts.js
+++ server/routers/posts.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getPosts} from '../controllers/posts.js'
+import { getPosts, createPosts } from '../controllers/posts.js'
+
 
 const router = express.Router();
 
 // router.get('/', (req, res) => { res.send('router success') })
 router.get("/", getPosts);
+router.post("/", createPosts);
 
 export default router
